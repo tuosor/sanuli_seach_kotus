d7060cb103afc066de9a8e3583af8dee87b03520
tuosor <49719870+tuosor@users.noreply.github.com>
2024-01-14 11:22:45 +0200

    New multichar search function. Also all characters to lower when reading Kotus data in.

--- search_kotus_hack.py
+++ search_kotus_hack.py
@@ -2,6 +2,7 @@
 # Read Kotus into dataframe to allow search based on letter locations.
 import numpy as np
 import pandas as pd
+import itertools
 
 # Function to filter according to number of characters
 def howManyLetters(df,num_letters):
@@ -10,7 +11,9 @@ def howManyLetters(df,num_letters):
       new_df = new_df[new_df[i] !='']
    return new_df
 
-# Function to filter based on possible location of character
+# Function to filter based on possible location of character.
+# Obsolete, since multiCharPossiblePlace can be run with
+# num_instances=1, which performs the same function.
 def charPossiblePlace(df,char,digits):
    new_df = df.copy(deep=True)
    total_df = None
@@ -20,7 +23,31 @@ def charPossiblePlace(df,char,digits):
       else: total_df = pd.concat([total_df,sub_df],axis=0)
    total_df = total_df.drop_duplicates()
    return total_df
-   
+
+
+# Function to filter based on possible location of character,
+# taking into account the possibility of two or more instances of it.
+def multiCharPossiblePlace(df,char,digits,num_instances):
+   new_df = df.copy(deep=True)
+   total_df = None
+   combinations = list(itertools.combinations(digits,num_instances))
+   for combination in combinations:
+      sub_df = None
+      for location in digits:
+         if location in combination:
+            if sub_df is None: sub_df = new_df[new_df[location]==char]
+            else: sub_df = sub_df[sub_df[location]==char]
+         else:
+            if sub_df is None: sub_df = new_df[new_df[location]!=char]
+            else: sub_df = sub_df[sub_df[location]!=char]
+      if total_df is None: total_df = sub_df
+      else: total_df = pd.concat([total_df,sub_df],axis=0)
+   # Digits tell the possible places, now get rid of impossible ones.
+   for col in total_df:
+      if col not in digits:
+         total_df = total_df[total_df[col]!=char]
+   return total_df
+
 
 KOTUS_FILE_PATH = 'kotus-sanalista_v1.xml'
 
@@ -41,11 +68,13 @@ for row in row_list:
 # Format to array
 arr = np.empty((len(parsed_row_list),max_letters),dtype='str')
 for i in range(0,len(parsed_row_list)):
-   row = list(parsed_row_list[i])
+   #row = list(parsed_row_list[i])
+   row = list(parsed_row_list[i].lower())
    arr[i, :len(row)] = row
 
 # Format to dataframe
 df = pd.DataFrame(data=arr)
+df = df.drop_duplicates()
 
 # Start filtering
 vowels = ['a','e','i','o','u','y','ä','ö','å']
@@ -87,3 +116,26 @@ new_df2 = charPossiblePlace(new_df2,'t',[0,3,4,5])
 new_df2 = charPossiblePlace(new_df2,'n',[0,3,4,5])
 
 print(new_df2) # One entry found: 'nainut'. Hooray!
+
+
+# Sanity check, 14.1.2024. Works as intended
+new_df3 = howManyLetters(df,4)
+new_df3 = charPossiblePlace(new_df3,'t',[0,2])
+new_df3 = charPossiblePlace(new_df3,'o',[1,3])
+# Returns many options
+
+new_df4 = howManyLetters(df,4)
+new_df4 = multiCharPossiblePlace(new_df4,'t',[0,2],2)
+new_df4 = multiCharPossiblePlace(new_df4,'o',[1,3],2)
+# Returns only "toto". Hooray!
+
+new_df5 = howManyLetters(df,6)
+new_df5 = multiCharPossiblePlace(new_df5,'t',[0,2,4],3)
+new_df5 = multiCharPossiblePlace(new_df5,'o',[1,3],2)
+# Returns only "totota". Hooray!
+
+new_df6 = howManyLetters(df,6)
+new_df6 = multiCharPossiblePlace(new_df6,'t',[0,2,4],2)
+new_df6 = multiCharPossiblePlace(new_df6,'o',[1,3],2)
+# Returns only "lotota". Hooray!
+
